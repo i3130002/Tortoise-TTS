8e1b3fe2501ddfe75737fcd7355cc8ae7c8563b8
i3130002 <i3130002@gmail.com>
2024-02-20 01:02:45 +0400

    Fix the wget

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@ git clone https://github.com/neonbjb/tortoise-tts &&
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 &&
 pip install transformers &&
 cd tortoise-tts && 
-python setup.py install
+python setup.py install &&
+mkdir tortoise/voices/i313002 &&
+cd tortoise/voices/i313002/ &&
+wget https://github.com/i3130002/Tortoise-TTS/raw/main/voice_sample.wav  -O 1.wav
 
