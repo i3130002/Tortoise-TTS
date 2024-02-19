4755618afda9ab6877e6424ec9a0937f2b34036c
i3130002 <i3130002@gmail.com>
2024-02-20 00:55:01 +0400

    Intial Setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-echo "Test"
+git clone https://github.com/neonbjb/tortoise-tts &&
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 &&
+pip install transformers &&
+cd tortoise-tts && 
+python setup.py install
+
